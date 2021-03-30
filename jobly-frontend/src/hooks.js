e21e55567a482ea6b8237f93f1ba4a99abd326c0
Sean Gibson <seanthomasgibson@gmail.com>
2021-03-29 21:00:26 -0700

    Added comments in jobly-frontend/src/hooks.js

--- jobly-frontend/src/hooks.js
+++ jobly-frontend/src/hooks.js
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
 
+/**
+ * Custom hook that acts as an interface between the app and local storage
+ * @param {string} key 
+ * @returns 3 functions to access an item in local storage
+ */
 const useLocalStorage = (key) => {
   const initVal = localStorage.getItem(key) || null;
   const [currItem, setCurrItem] = useState({ [key]: initVal });
 
+  /**
+   * Gets currItem, which reflects the item in local storage
+   * @returns item
+   */
   const getItem = () => currItem[key];
 
+  /**
+   * Sets item to newVal in local storage, and keeps track of its new value in
+   * state
+   * @param {any} newVal 
+   */
   const setItem = newVal => {
     localStorage.setItem(key, newVal);
     setCurrItem(currItem => currItem[key] = newVal);
   };
 
+  /**
+   * Removes item from local storage, and sets state to have value null
+   */
   const removeItem = () => {
     localStorage.removeItem(key);
     setCurrItem({ [key]: null });
